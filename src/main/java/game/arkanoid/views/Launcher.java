50f64a887908b746c504508a89d1a2dd6db527fb
Nguyễn Tùng <nvt2006hn1@gmail.com>
2025-10-21 16:44:33 +0700

    add sounds : menu, gamePlay, hitPaddle, brickBreak, loseLife

--- src/main/java/game/arkanoid/views/Launcher.java
+++
@@ -1,7 +0,0 @@
-package game.arkanoid.views;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Main.main(args); // gọi Application.launch(Main.class, args);
-    }
-}
