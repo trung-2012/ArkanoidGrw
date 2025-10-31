46b21641cc6901f61fd766e80756c6e83c59a384
Nguyễn Tùng <nvt2006hn1@gmail.com>
2025-10-31 18:34:49 +0700

    add pause menu sound

--- src/main/java/game/arkanoid/controllers/GameOverController.java
+++ src/main/java/game/arkanoid/controllers/GameOverController.java
@@ -1,13 +1,13 @@
 package game.arkanoid.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
--- src/main/java/game/arkanoid/controllers/StartMenuController.java
+++ src/main/java/game/arkanoid/controllers/StartMenuController.java
@@ -1,16 +1,17 @@
 package game.arkanoid.controllers;
 
+import game.arkanoid.sound.SoundManager;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 import java.io.IOException;
-import game.arkanoid.sound.SoundManager;
 
 public class StartMenuController {
 
